ba3db6b1839c4047a3a1711f4819a058e771ceb8
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-29 23:42:14 +0100

    task-4 fixed

--- js/task-4.js
+++ js/task-4.js
@@ -9,12 +9,14 @@ loginForm.addEventListener('submit', e => {
   const password = form.elements.password.value;
   const userData = { email, password };
 
+  userData.email = email.trim();
+  userData.password = password.trim();
+
   if (!email || !password) {
     return alert('All form fields must be filled in');
   }
 
-  userData.email = email.trim();
-  userData.password = password.trim();
   form.reset();
+  form.elements.email.focus();
   console.log(userData);
 });
