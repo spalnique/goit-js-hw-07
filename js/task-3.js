398b2c6917f74bdec18ecd419ce3ebb560e0850f
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-29 21:56:53 +0100

    task-3 fixed

--- js/task-3.js
+++ js/task-3.js
@@ -2,12 +2,10 @@ const inputElement = document.querySelector('#name-input');
 const spanElement = document.querySelector('#name-output');
 const headerElement = document.querySelector('h1');
 
-headerElement.innerHTML = spanElement.outerHTML;
-
 headerElement.classList.add('output-text');
 
-inputElement.addEventListener('input', event =>
-  event.currentTarget.value.trim()
-    ? (headerElement.textContent = event.currentTarget.value.trim())
-    : (headerElement.textContent = 'Anonymous'),
+inputElement.addEventListener('input', e =>
+  e.currentTarget.value.trim()
+    ? (spanElement.textContent = e.currentTarget.value.trim())
+    : (spanElement.textContent = 'Anonymous'),
 );
