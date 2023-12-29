15e67c05ce48e14ee95986d964ff8a4b8a79e670
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-29 15:47:01 +0100

    task-6 fixed

--- js/task-6.js
+++ js/task-6.js
@@ -8,34 +8,33 @@ const userInput = document.querySelector('#controls input');
 const createButton = document.querySelector('#controls [data-create]');
 const destroyButton = document.querySelector('#controls [data-destroy]');
 const divBoxes = document.querySelector('#boxes');
+
 userInput.setAttribute('type', 'text');
 divBoxes.style.visibility = 'hidden';
 
-userInput.addEventListener('input', e => {
-  const inputValue = e.currentTarget.value;
-  console.log(inputValue);
-});
-
-createButton.addEventListener('click', e => {
-  const input = Number(userInput.value);
-  if (input >= 1 && input <= 100) {
-    const newBlocks = Array.from({ length: input }, (_, i) => {
+function createBoxes(amount) {
+  amount = Number(userInput.value);
+  if (amount >= 1 && amount <= 100) {
+    const newBlocks = Array.from({ length: amount }, (_, i) => {
       const width = 30;
       const height = 30;
       return `<div class="new-box" style="width: ${width + 10 * i}px; height: ${
         height + 10 * i
       }px; background-color: ${getRandomHexColor()}"></div>`;
     });
-    newBlocks.forEach(x => x.replace(/\s+/g, ' '));
     divBoxes.insertAdjacentHTML('afterbegin', newBlocks.join('\n\n'));
     divBoxes.style.visibility = 'visible';
   } else {
     alert('Value should be from 1 to 100!');
     userInput.value = '';
   }
-});
+}
+
+
+createButton.addEventListener('click', createBoxes);
 
 destroyButton.addEventListener('click', e => {
+  divBoxes.innerHTML = '';
   divBoxes.style.visibility = 'hidden';
   userInput.value = '';
 });
