824180afe3f045bcda9114871d2d8007a55196e9
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-29 10:35:22 +0100

    task-4 complete

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+const userData = {};
+
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const form = e.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  const userData = { email, password };
+
+  if (!email || !password) {
+    return alert('All form fields must be filled in');
+  }
+
+  userData.email = email.trim();
+  userData.password = password.trim();
+  form.reset();
+  console.log(userData);
+  return userData;
+});
