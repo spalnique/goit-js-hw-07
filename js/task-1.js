8d0a68667816e382139676bcc72c4fb85dd445c4
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-30 00:55:53 +0100

    task-1 fixed

--- js/task-1.js
+++ js/task-1.js
@@ -3,13 +3,7 @@ console.log(`Number of categories: ${categoryList.length}`);
 
 categoryList.forEach(x => {
   console.log(`Category: ${x.querySelector('h2').textContent}`);
-  console.log(`Elements: ${x.querySelectorAll('li').length}`);
+  console.log(`Elements: ${x.querySelectorAll('ul > li').length}`);
 });
 
-// for (const each of categoryList) {
-//   console.log(
-//     `Category: ${each.querySelector('h2').textContent}\nElements: ${
-//       each.querySelectorAll('li').length
-//     }`,
-//   );
-// }
+// Усі ці властивості, по суті, є структурою DOM-дерева. Їх варто знати для розуміння DOM. Також питання про ці властивості можуть бути на співбесіді. На практиці, однак, вони застосовуються дуже рідко, оскільки вкладеність елементів і їх кількість може динамічно змінюватись. Це робить код з використанням цих властивостей ненадійним.
--- js/task-6.js
+++ js/task-6.js
@@ -1,41 +1,35 @@
+const userInput = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-const userInput = document.querySelector('#controls input');
-const createButton = document.querySelector('#controls [data-create]');
-const destroyButton = document.querySelector('#controls [data-destroy]');
-const divBoxes = document.querySelector('#boxes');
-
-userInput.setAttribute('type', 'text');
-divBoxes.style.visibility = 'hidden';
-
 function createBoxes() {
-  divBoxes.innerHTML = '';
+  boxesContainer.textContent = '';
   if (userInput.value >= 1 && userInput.value <= 100) {
     const newBlocks = Array.from({ length: userInput.value }, (_, i) => {
       return `<div class="new-box" style="width: ${30 + 10 * i}px;
                                           height: ${30 + 10 * i}px;
                                           background-color: ${getRandomHexColor()}">
-                                          </div>`;
+                                          </div>`;  
     });
-    divBoxes.insertAdjacentHTML('afterbegin', newBlocks.join('\n\n'));
-    divBoxes.style.visibility = 'visible';
+    boxesContainer.insertAdjacentHTML('afterbegin', newBlocks.join('\n\n'));
     userInput.value = '';
   } else {
     alert('Enter a value from 1 to 100!');
-    divBoxes.innerHTML = '';
-    divBoxes.style.visibility = 'hidden';
+    boxesContainer.textContent = '';
     userInput.value = '';
   }
 }
 
 createButton.addEventListener('click', createBoxes);
 destroyButton.addEventListener('click', e => {
-  divBoxes.innerHTML = '';
-  divBoxes.style.visibility = 'hidden';
+  boxesContainer.textContent = '';
   userInput.value = '';
 });
 userInput.addEventListener('keydown', e => {
