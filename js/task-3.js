5d7dd8cc942125aa9883c61c2e6baac3aa77a395
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-30 01:36:14 +0100

    task-3 fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,8 @@
 const input = document.querySelector('#name-input');
-const span = document.querySelector('#name-output');
-const header = document.querySelector('h1');
-
-header.classList.add('output-text');
+const output = document.querySelector('#name-output');
 
 input.addEventListener('input', e =>
   e.currentTarget.value.trim()
-    ? (span.textContent = e.currentTarget.value.trim())
-    : (span.textContent = 'Anonymous'),
+    ? (output.textContent = e.currentTarget.value.trim())
+    : (output.textContent = 'Anonymous'),
 );
