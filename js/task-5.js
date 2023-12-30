659afa374bc0c7777976b782d914a890738afdd8
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-30 02:15:52 +0100

    task-5 fixed

--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,17 @@
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const output = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-const body = document.querySelector('body');
-const button = document.querySelector('.change-color');
-const output = document.querySelector('.color');
-
-button.addEventListener('click', _ => {
+function changeColor() {
   const newColor = getRandomHexColor();
   body.style.backgroundColor = newColor;
   output.textContent = newColor;
-});
+}
+
+button.addEventListener('click', changeColor);
